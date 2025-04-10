01a771632886115ca8c6ab68e8d00d29a2ddc3ef
LennartHemme <lennart.hemme@radioemscherlippe.de>
2025-04-10 14:46:10 +0200

    Delete api directory

--- api/vorlage.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  const vorlage = `Bitte schreibe ein kurzes Radioupdate anhand der gewählten Artikel. Achte darauf, dass es einen roten Faden gibt und der Moderator informiert und freundlich klingt. Nutze gerne Alltagssprache.`;
-  res.status(200).send(vorlage);
-}
